c8281dff4e77858c3858efbfd335b6dd6ab747ed
Jay Ganatra <ganatrajay2000@gmail.com>
2024-09-19 13:50:10 -0400

    raw data

---
+++ charts_api/raw_data.py
@@ -0,0 +1,27 @@
+candlestick_chart = [
+    {"x": "2023-01-01", "open": 30, "high": 40, "low": 25, "close": 35},
+    {"x": "2023-01-02", "open": 35, "high": 45, "low": 30, "close": 40},
+    {"x": "2023-01-03", "open": 40, "high": 50, "low": 35, "close": 45},
+]
+
+line_chart = {
+    "labels": ["Jan", "Feb", "Mar", "Apr"],
+    "data": [10, 20, 30, 40]
+}
+
+bar_chart = {
+    "labels": ["Product A", "Product B", "Product C"],
+    "data": [100, 150, 200]
+}
+
+pie_chart = {
+    "labels": ["Red", "Blue", "Yellow"],
+    "data": [300, 50, 100]
+}
+
+allData = {
+    "candlestick_chart": candlestick_chart,
+    "line_chart": line_chart,
+    "bar_chart": bar_chart,
+    "pie_chart": pie_chart
+}

e4385ab6b27a31df50914926d27bb6934a32cd1c
Jay Ganatra <ganatrajay2000@gmail.com>
2024-09-19 13:45:35 -0400

    fix spelling

--- charts_api/views.py
+++ charts_api/views.py
@@ -44,7 +44,7 @@ def pie_chart_data(request):
 @api_view(['GET'])
 def all_data(request):
     data = {
-        "candlestick": [
+        "candlestick_chart": [
             {"x": "2023-01-01", "open": 30, "high": 40, "low": 25, "close": 35},
             {"x": "2023-01-02", "open": 35, "high": 45, "low": 30, "close": 40},
             {"x": "2023-01-03", "open": 40, "high": 50, "low": 35, "close": 45},

c0404b3d0a94ce50971e04b0c445a01685be6e12
Jay Ganatra <ganatrajay2000@gmail.com>
2024-09-19 14:33:32 -0400

    basic unit tests

--- charts_api/tests.py
+++ charts_api/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+# tests.py in your Django app
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ChartAPITests(APITestCase):
+    def test_candlestick_data(self):
+        url = reverse('candlestick_data')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, list)
+
+    def test_line_chart_data(self):
+        url = reverse('line_chart_data')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
+
+    def test_bar_chart_data(self):
+        url = reverse('bar_chart_data')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
+
+    def test_pie_chart_data(self):
+        url = reverse('pie_chart_data')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
+
+    def test_all_data(self):
+        url = reverse('all_data')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
--- charts_api/urls.py
+++ charts_api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import candlestick_data, line_chart_data, bar_chart_data, pie_chart_data, all_data
 
 urlpatterns = [
-    path('all-data/', all_data, name='all-data'),
-    path('candlestick-data/', candlestick_data, name='candlestick-data'),
-    path('line-chart-data/', line_chart_data, name='line-chart-data'),
-    path('bar-chart-data/', bar_chart_data, name='bar-chart-data'),
-    path('pie-chart-data/', pie_chart_data, name='pie-chart-data'),
+    path('all-data/', all_data, name='all_data'),
+    path('candlestick-data/', candlestick_data, name='candlestick_data'),
+    path('line-chart-data/', line_chart_data, name='line_chart_data'),
+    path('bar-chart-data/', bar_chart_data, name='bar_chart_data'),
+    path('pie-chart-data/', pie_chart_data, name='pie_chart_data'),
 ]
